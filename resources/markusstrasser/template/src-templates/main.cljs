c0311e20838b5634f80398fa116e786e477d28a2
Markus Strasser <strasser.ms@gmail.com>
2025-10-27 17:59:10 -0700

    Fix unused binding warning in template

--- resources/markusstrasser/template/src-templates/main.cljs
+++ resources/markusstrasser/template/src-templates/main.cljs
@@ -6,7 +6,7 @@
 
 (defn app-component
   "Root application component"
-  [{:keys [db]}]
+  [_state]
   [:div {:class "min-h-screen flex items-center justify-center"}
    [:div {:class "text-center"}
     [:h1 {:class "text-4xl font-bold text-gray-900 mb-4"}
